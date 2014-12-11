1a43fc998d30ae8fb6a02bf11699c4a7c30dba7d
Eslam A. Hefnawy <eahefnawy@gmail.com>
2014-12-11 23:49:40 +0200

    finishing up

--- client/spinner.js
+++ client/spinner.js
@@ -12,7 +12,7 @@ Meteor.Spinner.options = {
     shadow: false, // Whether to render a shadow
     hwaccel: false, // Whether to use hardware acceleration
     className: 'spinner', // The CSS class to assign to the spinner
-    zIndex: 2e9 // The z-index (defaults to 2000000000)
-    //top: 'auto', // Top position relative to parent in px
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: '380px' // Top position relative to parent in px
     //left: 'auto' // Left position relative to parent in px
 };
